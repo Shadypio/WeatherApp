49e2c64d3f1f5dbf7af6313e6766ff12983a2bcf
Shadypio <enzuccioamendola@gmail.com>
2023-05-05 15:45:24 +0200

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
+  </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import SiteNavigation from './components/SiteNavigation.vue';
+
 
 </script>
 
